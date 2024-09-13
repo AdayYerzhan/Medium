5ce7319ffb0295ddc53b08168829f747b06608cf
Yerzhan <yerzhan.aday@gmail.com>
2024-09-13 07:47:56 +0500

    Улучшение компнента TopBar

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Link, NavLink, Outlet} from "react-router-dom";
+import {CurrentUserContext} from "../constexts/currentUser";
 
 const TopBar = () => {
+    const [currentUserState] = useContext(CurrentUserContext);
+
     return (
         <div>
             <nav className="navbar navbar-light">
@@ -15,16 +18,39 @@ const TopBar = () => {
                                 Home
                             </NavLink>
                         </li>
-                        <li className="nav-item">
-                            <NavLink to="/login" className="nav-link">
-                                Sign in
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/register" className="nav-link">
-                                Sign up
-                            </NavLink>
-                        </li>
+                        {!currentUserState.isLoggedIn && (
+                            <>
+                                <li className="nav-item">
+                                    <NavLink to="/login" className="nav-link">
+                                        Sign in
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/register" className="nav-link">
+                                        Sign up
+                                    </NavLink>
+                                </li>
+                            </>
+                        )}
+                        {currentUserState.isLoggedIn && (
+                            <>
+                                <li className="nav-item">
+                                    <NavLink to="/articles/new" className="nav-link">
+                                        <i className="ion-compose"></i>
+                                        &nbsp;New Post
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        to={`/profiles/${currentUserState.currentUser.username}`}
+                                        className="nav-link"
+                                    >
+                                        <img className="user-pic" src={currentUserState.currentUser.image} alt="img" />
+                                        &nbsp; {currentUserState.currentUser.username}
+                                    </NavLink>
+                                </li>
+                            </>
+                        )}
                     </ul>
                 </div>
             </nav>
