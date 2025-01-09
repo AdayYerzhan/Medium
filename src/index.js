c4c6346c16ded41dab0bc9ffa36e0b3a50b606f7
Yerzhan <yerzhan.aday@gmail.com>
2025-01-09 22:54:24 +0500

    Создан черновик APP

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider} from "react-router-dom";
-
-import routes from "routes";
-import {CurrentUserProvider} from "constexts/currentUser";
-import CurrentUserChecker from "components/currentUserChecker";
 
 const App = () => {
     return (
-        <CurrentUserProvider>
-            <CurrentUserChecker>
-                <RouterProvider router={routes()} />
-             </CurrentUserChecker>
-        </CurrentUserProvider>
+        <h1>Hello world!</h1>
     );
 }
 
